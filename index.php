2d203f1eafe13bc334b515b194bb5a1145365cc2
devixu <devixu@outlook.com>
2017-09-03 07:43:05 +0800

    第一次完本，提交到git 客户端放到user目录下 数据库和后台管理都做了密码访问限制

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+include_once 'lib/config.php';
+include_once 'header.php';
+?>
+
+
+<div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <h1 class="header center orange-text"><?php echo $site_name; ?></h1>
+            <div class="row center">
+                <h5 class="header col s12 light">企业外贸VPN，不对外商用</h5>
+            </div>
+            <div class="row center">
+                <a href="user/register.php" id="download-button" class="btn-large waves-effect waves-light orange">立即注册</a>
+            </div>
+            <br><br>
+        </div>
+</div>
+
+
+<div class="container">
+    <div class="section">
+
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+                    <h5 class="center">超级快</h5>
+
+                    <p class="light">
+                        使用异步I/O和事件驱动编程
+                    </p>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+                    <h5 class="center">开源</h5>
+
+                    <p class="light">
+                        完全免费开源，一个致力于提供无错误代码和长期支持全球性社区
+                    </p>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+                    <h5 class="center">易于使用</h5>
+
+                    <p class="light">
+                        可用于多个平台，包括PC,MAC,移动(Android和ios)和路由器(OpenWRT)
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <br><br>
+
+    <div class="section">
+
+    </div>
+</div>
+<?php  include_once 'ana.php';
+       include_once 'footer.php';?>
